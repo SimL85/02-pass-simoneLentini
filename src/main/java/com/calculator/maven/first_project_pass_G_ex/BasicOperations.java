80bcf32680c9ca773061da515172ef47815ce544
Simone L <lentini.simone@gmail.com>
2021-03-27 17:27:53 +0100

    created BasicOpreations interface

--- src/main/java/com/calculator/maven/first_project_pass_G_ex/BasicOperations.java
+++ src/main/java/com/calculator/maven/first_project_pass_G_ex/BasicOperations.java
@@ -1,11 +1,12 @@
 package com.calculator.maven.first_project_pass_G_ex;
 
-public interface BasicMathOperations {
+public interface BasicOperations {
 	public double addition(double firstNumber, double secondNumber);
 
 	public double subtraction(double firstNumber, double secondNumber);
 
+	public double moltiplication(double firstNumber, double secondNumber);
+
 	public double division(double firstNumber, double secondNumber);
 
-	public double moltiplication(double firstNumber, double secondNumber);
 }
