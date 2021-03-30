14d11840a8b1efaef865db91938ccf7b09835bd0
Simone L <lentini.simone@gmail.com>
2021-03-30 14:23:05 +0200

    fixed few issues to the test case AdvancedCalculator

--- src/test/java/com/calculator/maven/first_project_pass_G_ex/AdvancedCalculatorTest.java
+++ src/test/java/com/calculator/maven/first_project_pass_G_ex/AdvancedCalculatorTest.java
@@ -13,61 +13,139 @@ public class AdvancedCalculatorTest {
 	public static final Logger LOG = Logger.getLogger(AdvancedCalculator.class.getName());
 
 	@Test
-	public void testSquare() {
-		double number = input.nextDouble() * 10;
+	public void testSquarePn() {
+		double number = input.nextDouble();
+		double result = number * number;
+		LOG.info("Testing the method square with a positive number: " + number + "\n");
+		assertEquals(calc.square(number), result, 0);
+
+	}
+
+	@Test
+	public void testSquareNn() {
+		double number = input.nextDouble() * -1;
+		double result = number * number;
+		LOG.info("Testing the method square with a negative number: " + number + "\n");
+		assertEquals(calc.square(number), result, 0);
+
+	}
+
+	@Test
+	public void testSquareZero() {
+		double number = 0;
 		double result = number * number;
 		LOG.info("Testing the method square of: " + number + "\n");
 		assertEquals(calc.square(number), result, 0);
-		LOG.info("Testing the method square of: " + -2.0 + "\n");
-		assertEquals(calc.square(-2.0), 4.0, 0);
-		LOG.info("Testing the method square of: " + 0.0 + "\n");
-		assertEquals(calc.square(0.0), 0.0, 0);
 
 	}
 
 	@Test
-	public void testRaisedToThePower() {
-		double base = input.nextDouble() * 10;
-		double exponent = input.nextDouble() * 10;
+	public void testRaisedToThePowerPn() {
+		double base = input.nextDouble();
+		double exponent = input.nextDouble();
 		double result = Math.pow(base, exponent);
 		LOG.info("Testing the method raisedToThePower with base :" + base + "and exponent " + exponent + "\n");
 		assertEquals(calc.raisedToThePower(base, exponent), result, 0);
-		LOG.info("Testing the method raisedToThePower with base :" + -100.0 + "and exponent " + 0.0 + "\n");
-		assertEquals(calc.raisedToThePower(-100.0, 0.0), 1.0, 0);
-		LOG.info("Testing the method raisedToThePower with base :" + 0.0 + "and exponent " + 0.0 + "\n");
-		assertEquals(calc.raisedToThePower(0.0, 0.0), 1.0, 0);
 
 	}
 
 	@Test
-	public void testRemainder() {
-		double dividend = input.nextDouble() * 10;
-		double divisor = input.nextDouble() * 10;
+	public void testRaisedToThePowerNn() {
+		double base = input.nextDouble() * -1;
+		double exponent = input.nextDouble() * -1;
+		double result = Math.pow(base, exponent);
+		LOG.info("Testing the method raisedToThePower with base(negative number) :" + base + "and exponent(negative number) "
+				+ exponent + "\n");
+		assertEquals(calc.raisedToThePower(base, exponent), result, 0);
+
+	}
+
+	@Test
+	public void testRaisedToThePowerZero() {
+		double base = 0;
+		double exponent = 0;
+		double result = Math.pow(base, exponent);
+		LOG.info("Testing the method raisedToThePower with base :" + base + "and exponent " + exponent + "\n");
+		assertEquals(calc.raisedToThePower(base, exponent), result, 0);
+
+	}
+
+	@Test
+	public void testRemainderPn() {
+		double dividend = input.nextDouble();
+		double divisor = input.nextDouble();
 		double result = dividend % divisor;
 		LOG.info("Testing the method remainder with dividend: " + dividend + " and divisor " + divisor + "\n");
 		assertEquals(calc.remainder(dividend, divisor), result, 0);
-		LOG.info("Testing the method remainder with dividend: " + -20.0 + " and divisor " + -10.0 + "\n");
-		assertEquals(calc.remainder(-20.0, -10.0), 0.0, 0);
-		LOG.info("Testing the method remainder with dividend: " + 0.0 + " and divisor" + 0.0 + "\n");
-		assertEquals(calc.remainder(0.0, 1.0), 0.0, 0);
+		
 
 	}
 	@Test
-	public void testRoot() {
-		double base = input.nextDouble() * 10;
-		double exponent = input.nextDouble() * 10;
-		double result = Math.pow(base,1.0/ exponent);
+	public void testRemainderNn() {
+		double dividend = input.nextDouble()*-1;
+		double divisor = input.nextDouble()*-1;
+		double result = dividend % divisor;
+		LOG.info("Testing the method remainder with dividend(negative number): " + dividend + " and divisor(negative number) " + divisor + "\n");
+		assertEquals(calc.remainder(dividend, divisor), result, 0);
+		
+
+	}
+	@Test
+	public void testRemainderZero() {
+		double dividend = 0;
+		double divisor = 0;
+		double result = dividend % divisor;
+		LOG.info("Testing the method remainder with dividend: " + dividend + " and divisor " + divisor + "\n");
+		assertEquals(calc.remainder(dividend, divisor), result, 0);
+		
+
+	}
+	@Test
+	public void testRootPn() {
+		double base = input.nextDouble();
+		double exponent = input.nextDouble();
+		double result = Math.pow(base, 1.0 / exponent);
 		LOG.info("Testing the method root with base :" + base + "and exponent " + exponent + "\n");
 		assertEquals(calc.root(base, exponent), result, 0);
-		
-	
 
 	}
 	@Test
-	public void testAbsoluteValue() {
-		double number = input.nextDouble() * -10;
+	public void testRootNn() {
+		double base = input.nextDouble() * -1;
+		double exponent = input.nextDouble() * -1;
+		double result = Math.pow(base, 1.0 / exponent);
+		LOG.info("Testing the method root with base(negative number):" + base + "and exponent(negative number) " + exponent + "\n");
+		assertEquals(calc.root(base, exponent), result, 0);
+
+	}
+	@Test
+	public void testRootZero() {
+		double base = 0;
+		double exponent = 0;
+		double result = Math.pow(base, 1.0 / exponent);
+		LOG.info("Testing the method root with base :" + base + "and exponent " + exponent + "\n");
+		assertEquals(calc.root(base, exponent), result, 0);
+
+	}
+	@Test
+	public void testAbsoluteValueNn() {
+		double number = input.nextDouble() * -1;
 		double result = Math.abs(number);
-		LOG.info("Testing the method absoluteValue:" + number +"\n");
+		LOG.info("Testing the method absoluteValue(negative number):" + number + "\n");
+		assertEquals(calc.absoluteValue(number), result, 0);
+	}
+	@Test
+	public void testAbsoluteValuePn() {
+		double number = input.nextDouble();
+		double result = Math.abs(number);
+		LOG.info("Testing the method absoluteValue:" + number + "\n");
+		assertEquals(calc.absoluteValue(number), result, 0);
+	}
+	@Test
+	public void testAbsoluteValueZero() {
+		double number = 0;
+		double result = Math.abs(number);
+		LOG.info("Testing the method absoluteValue:" + number + "\n");
 		assertEquals(calc.absoluteValue(number), result, 0);
 	}
 }
